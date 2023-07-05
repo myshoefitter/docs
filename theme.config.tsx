558819df7f8f0b987ae00cf905662716e6281779
Marius C. Bolik <marius@eyloo.com>
2023-07-05 09:26:42 +0200

    chore: tidy up a bit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <img src="https://uploads-ssl.webflow.com/627136f58be1570905eb8a28/6458f00e1462140aca5e9f19_mySHOEFITTER%20Logo-p-500.png" width="180px" />,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/myshoefitter/',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // chat: {
+  //   link: 'https://discord.com',
+  // },
+  docsRepositoryBase: 'https://github.com/myshoefitter/',
   footer: {
-    text: 'Nextra Docs Template',
+    text: '© mySHOEFITTER GmbH',
   },
+  darkMode: true
 }
 
 export default config
