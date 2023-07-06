c879ced23732365fa6d2218fee566a3e02b4d76d
Marius C. Bolik <marius@eyloo.com>
2023-07-06 10:49:58 +0200

    style: add global styles

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <img src="https://uploads-ssl.webflow.com/627136f58be1570905eb8a28/6458f00e1462140aca5e9f19_mySHOEFITTER%20Logo-p-500.png" width="180px" />,
+  logo: <img src="https://uploads-ssl.webflow.com/627136f58be1570905eb8a28/6458f00e1462140aca5e9f19_mySHOEFITTER%20Logo-p-500.png" width="160px" />,
   project: {
     link: 'https://github.com/myshoefitter/',
   },
@@ -15,7 +15,7 @@ const config: DocsThemeConfig = {
   },
   nextThemes: {
     defaultTheme: 'light'
-  }
+  },
 }
 
 export default config
