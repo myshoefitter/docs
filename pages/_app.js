1d338f42231f44ae1f823d64f453806c463a0b39
Marius C. Bolik <marius@eyloo.com>
2023-08-24 09:33:27 +0200

    chore(analytics): add pirsch

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
-import '../styles.scss';
+import "../styles.scss";
+import Script from "next/script";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script defer src="https://api.pirsch.io/pirsch-extended.js" id="pirschextendedjs" data-code="7pFbidsnF9q54fWWS9MoOitwQvddJxwm" />
+      <Component {...pageProps} />
+    </>
+  );
 }
