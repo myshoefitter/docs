eb6f424c2b0e05cb292d8ed546465fbabf8f4403
Marius Bolik <marius@eyloo.com>
2023-09-26 14:04:36 +0200

    docs: update integration guide

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <img src="https://uploads-ssl.webflow.com/627136f58be1570905eb8a28/6458f00e1462140aca5e9f19_mySHOEFITTER%20Logo-p-500.png" width="160px" />,
   project: {
-    link: 'https://github.com/myshoefitter/',
+    link: 'https://github.com/myshoefitter/js-sdk',
   },
   // chat: {
   //   link: 'https://discord.com',
