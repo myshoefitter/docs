f23148588310d7b4f0c7542fbef08a279fc52e7e
Marius C. Bolik <marius@eyloo.com>
2023-11-16 19:47:20 +0100

    chore(seo): add meta tags

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      defaultTitle: 'mySHOEFITTER Docs',
+      titleTemplate: '%s – mySHOEFITTER',
+      description: 'Add mySHOEFITTER to your Shop.'
+    }
+  },
   logo: <img src="https://uploads-ssl.webflow.com/627136f58be1570905eb8a28/6458f00e1462140aca5e9f19_mySHOEFITTER%20Logo-p-500.png" width="160px" />,
   project: {
     link: 'https://github.com/myshoefitter/js-sdk',
